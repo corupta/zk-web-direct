2b4a64105515d52f37974338b75f4d39ca382c52
Egemen Göl <egemengol@gmail.com>
2024-10-22 15:32:05 +0300

    vite config for multipage

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,33 @@ export default defineConfig({
       "Cross-Origin-Embedder-Policy": "require-corp",
     },
   },
+  // root: "./src",
+  build: {
+    outDir: "./dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: "./index.html",
+        "heavy-with-worker": "./heavy-with-worker.html",
+        "heavy-without-worker": "./heavy-without-worker.html",
+        "light-with-worker": "./light-with-worker.html",
+        "light-without-worker": "./light-without-worker.html",
+      },
+    },
+  },
 });
+
+// export default {
+//   root: "./src",
+//   build: {
+//     outDir: "../dist",
+//     emptyOutDir: true,
+//     rollupOptions: {
+//       input: {
+//         index: "./src/index.html",
+//         about: "./src/about.html",
+//         article: "./src/blog/article.html",
+//       },
+//     },
+//   },
+// };
